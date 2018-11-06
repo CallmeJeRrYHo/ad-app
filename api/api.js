476993e5cea67fcfd0910893024d304cded67b4f
pandalord <330213905@qq.com>
2018-11-06 21:57:45 +0800

    增加多个查询

--- api/api.js
+++ api/api.js
@@ -4,7 +4,7 @@
 	(global.api = factory());
 }(this, (function () {
     var api = {
-        HOST: 'http://219.132.139.116:9090',
+        HOST: 'http://39.108.245.130:9090', // http://39.108.245.130:9090 http://219.132.139.116:9090
         // 公告类
         selectMsg: function (data) {
             return apiHttp.fetchPost(apiConfig.selectMsg, data);
